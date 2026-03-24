a574a63624f8e8f1ef641bf22191cb5ef2700110
luk4w <luk4w@outlook.com>
2026-03-24 00:08:16 -0300

    validacao de argumentos e extensao

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,25 @@
 // Grupo: RA1 10
 
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    cout << "ASMNATOR: Analisador Lexico e Gerador de Assembly para ARMv7" << endl;
+    // Validacao do numero de argumentos
+    if (argc != 2) {
+        cerr << "Numero de argumentos invalidos\n";
+        cerr << "Uso: " << argv[0] << " <teste.txt>\n";
+        return 1;
+    }
+
+    string arq = argv[1];
+
+    // Validacao de extensao .txt
+    if (arq.length() < 4 || arq.substr(arq.length() - 4) != ".txt") {
+        cerr << "A extensao do arquivo de entrada deve ser .txt\n";
+        return 1;
+    }
+    
     return 0;
 }
