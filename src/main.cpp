24d78253a96da67243a1ba053fa389c676e4dc1a
luk4w <luk4w@outlook.com>
2026-03-24 00:30:19 -0300

    add lerArquivo

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,17 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+
+#include "cli_controller.hpp"
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     // Validacao do numero de argumentos
-    if (argc != 2) {
+    if (argc != 2)
+    {
         cerr << "Numero de argumentos invalidos\n";
         cerr << "Uso: " << argv[0] << " <teste.txt>\n";
         return 1;
@@ -17,10 +22,22 @@ int main(int argc, char* argv[]) {
     string arq = argv[1];
 
     // Validacao de extensao .txt
-    if (arq.length() < 4 || arq.substr(arq.length() - 4) != ".txt") {
+    if (arq.length() < 4 || arq.substr(arq.length() - 4) != ".txt")
+    {
         cerr << "A extensao do arquivo de entrada deve ser .txt\n";
         return 1;
     }
-    
+
+    // Alocar memoria dinamica para o buffer de linhas
+    std::vector<std::string> buffer_linhas;
+
+    // Extrair os dados do arquivo e armazenar no buffer
+    int status = lerArquivo(arq, buffer_linhas);
+    if (status != 0)
+    {
+        std::cerr << "Falha ao ler o arquivo " << arq << "\n";
+        return status;
+    }
+
     return 0;
 }
